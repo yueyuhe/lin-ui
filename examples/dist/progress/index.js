f1248ab30da1891ba04aaaa203fab4fee9810d77
mario.zhang <525349965@qq.com>
2020-04-05 14:45:54 +0800

    feat(progress): 调整demo;优化代码

--- examples/dist/progress/index.js
+++ examples/dist/progress/index.js
@@ -7,7 +7,7 @@ Component({
   /**
    * 组件的属性列表
    */
-  externalClasses: ['l-class', 'l-text-class', 'l-slot-class', 'l-active-class', 'l-background-class'],
+  externalClasses: ['l-class', 'l-text-class', 'l-active-class', 'l-background-class'],
   behaviors: [validator],
   properties: {
     percent: {
@@ -38,62 +38,59 @@ Component({
       value: 'right',
       options: ['left', 'right']
     },
-    slotWidth: {
-      type: Number,
-      value: 0
-    },
-    slotHeight: {
-      type: Number,
-      value: 0
-    },
     textColor: {
       type: String,
-      value: '#000000'
+    },
+    interval: {
+      type: Number,
+      value: 20,
     }
   },
 
   options: {
-    multipleSlots: true
+    multipleSlots: true,
+    pureDataPattern: /^_/ // 指定所有 _ 开头的数据字段为纯数据字段
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    slot_width: null,
-    slot_height: null,
-    progress_width: null,
-    progress_height: null,
-    margin_bottom: 0,
-    margin_left: 0,
+    _slotWidth: 0,
+    _slotHeight: 0,
+    _progressWidth: 0,
+    _progressHeight: 0,
+    _marginBottom: 0,
+    _marginLeft: 0,
+    _marginTop: 0,
   },
 
   observers: {
-    'slot_width, slot_height, progress_width, progress_height, percent': function (slot_width, slot_height, progress_width, progress_height, percent) {
-      let margin_top = -(slot_height - progress_height) / 2
-      let margin_left = (progress_width - slot_width) * percent / 100
+    '_slotWidth, _slotHeight, _progressWidth, _progressHeight, percent': function (_slotWidth, _slotHeight, _progressWidth, _progressHeight, percent) {
+      const _marginTop = -(_slotHeight - _progressHeight) / 2
+      const _marginLeft = (_progressWidth - _slotWidth) * percent / 100
       this.setData({
-        margin_top,
-        margin_left
+        _marginTop,
+        _marginLeft
       })
     }
   },
 
   lifetimes: {
     attached() {
-      let that = this
+      const that = this
       const query_slot = wx.createSelectorQuery().in(this)
       query_slot.select('.slot').boundingClientRect(function (res) {
         that.setData({
-          slot_width: px2rpx(res.width),
-          slot_height: px2rpx(res.height)
+          _slotWidth: px2rpx(res.width),
+          _slotHeight: px2rpx(res.height)
         })
       }).exec()
       const query_progress = wx.createSelectorQuery().in(this)
       query_progress.select('.progress').boundingClientRect(function (res) {
         that.setData({
-          progress_height: px2rpx(res.height),
-          progress_width: px2rpx(res.width)
+          _progressHeight: px2rpx(res.height),
+          _progressWidth: px2rpx(res.width)
         })
       }).exec()
     }
