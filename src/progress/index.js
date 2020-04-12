b052721b4876e54f1dd28b83de6ff18f0318a1c6
mario.zhang <525349965@qq.com>
2020-04-12 17:28:57 +0800

    feat(progress): 优化progress组件

--- src/progress/index.js
+++ src/progress/index.js
@@ -1,13 +1,13 @@
 // components/progress/index.js
 import {
   px2rpx
-} from '../utils/util.js'
-import validator from '../behaviors/validator';
+} from "../utils/util.js"
+import validator from "../behaviors/validator";
 Component({
   /**
    * 组件的属性列表
    */
-  externalClasses: ['l-class', 'l-text-class', 'l-active-class', 'l-background-class'],
+  externalClasses: ["l-class", "l-text-class", "l-active-class", "l-background-class"],
   behaviors: [validator],
   properties: {
     percent: {
@@ -20,14 +20,14 @@ Component({
     },
     borderRadius: {
       type: Number,
-      value: 0,
+      value: 6,
     },
     activeColor: {
       type: String,
     },
     backgroundColor: {
       type: String,
-      value: '#EBEBEB',
+      value: "#EBEBEB",
     },
     showInfo: {
       type: Boolean,
@@ -35,8 +35,8 @@ Component({
     },
     textPosition: {
       type: String,
-      value: 'right',
-      options: ['left', 'right']
+      value: "right",
+      options: ["left", "right", "inner"]
     },
     textColor: {
       type: String,
@@ -66,7 +66,7 @@ Component({
   },
 
   observers: {
-    '_slotWidth, _slotHeight, _progressWidth, _progressHeight, percent': function (_slotWidth, _slotHeight, _progressWidth, _progressHeight, percent) {
+    "_slotWidth, _slotHeight, _progressWidth, _progressHeight, percent": function (_slotWidth, _slotHeight, _progressWidth, _progressHeight, percent) {
       const _marginTop = -(_slotHeight - _progressHeight) / 2
       const _marginLeft = (_progressWidth - _slotWidth) * percent / 100
       this.setData({
@@ -79,14 +79,14 @@ Component({
   lifetimes: {
     attached() {
       const querySlot = wx.createSelectorQuery().in(this)
-      querySlot.select('.slot').boundingClientRect(res => {
+      querySlot.select(".slot").boundingClientRect(res => {
         this.setData({
           _slotWidth: px2rpx(res.width),
           _slotHeight: px2rpx(res.height)
         })
       }).exec()
       const queryProgress = wx.createSelectorQuery().in(this)
-      queryProgress.select('.progress').boundingClientRect(res => {
+      queryProgress.select(".progress").boundingClientRect(res => {
         this.setData({
           _progressHeight: px2rpx(res.height),
           _progressWidth: px2rpx(res.width)
@@ -98,7 +98,5 @@ Component({
   /**
    * 组件的方法列表
    */
-  methods: {
-
-  }
+  methods: {}
 })
